957ce95df47ed5fb78f63a53355ea9f0190e8a94
YoshitakaKyougoku <114034035+YoshitakaKyougoku@users.noreply.github.com>
2024-05-02 16:47:47 +0900

    バリデーションとスタイル

--- src/components/newThread/newThread.tsx
+++ src/components/newThread/newThread.tsx
@@ -1,50 +1,56 @@
 import { FormEventHandler, useState } from "react";
 import styles from "./newThread.module.css";
 const url = "https://railway.bulletinboard.techtrain.dev/threads";
-const createNewThread: FormEventHandler<HTMLFormElement> = (event) => {
-  event.preventDefault();
-  const form = new FormData(event.currentTarget);
-  console.log("fetch");
-  fetch(url, {
-    method: "POST",
-    body: JSON.stringify({
-      title: form.get("title") || "",
-    }),
-  })
-    .then(async (res) => {
-      const data = await res.json();
-      console.log(data);
-    })
-    .catch(() => {
-      console.log("error");
-    });
-};
 const handleInvalid: FormEventHandler<HTMLFormElement> = (event) => {
   event.preventDefault();
   alert("タイトルを入力してください");
 };
 function NewThread() {
+  const [title, setTitle] = useState("");
+  const createNewThread: FormEventHandler<HTMLFormElement> = (event) => {
+    event.preventDefault();
+    const form = new FormData(event.currentTarget);
+    console.log("fetch");
+    fetch(url, {
+      method: "POST",
+      body: JSON.stringify({
+        title: form.get("title") || "",
+      }),
+    })
+      .then(async (res) => {
+        const data = await res.json();
+        console.log(data);
+        setTitle("");
+      })
+      .catch(() => {
+        console.log("error");
+      });
+  };
   return (
     <section className={styles.container}>
-      <h1 className={styles.title}>スレッド新規作成</h1>
-      <form onSubmit={createNewThread} onInvalid={handleInvalid}>
-        <label className={styles.inputTitle} htmlFor="title">
-          <div className={styles.inputArea}>
-            {/* <span className={styles.errorMessage}>
-              タイトルを入力してください
-            </span> */}
+      <div className={styles.box}>
+        <h1 className={styles.title}>スレッド新規作成</h1>
+        <form
+          className={styles.form}
+          onSubmit={createNewThread}
+          onInvalid={handleInvalid}
+        >
+          <label className={styles.inputArea} htmlFor="title">
             <input
               className={styles.input}
               type="text"
               name="title"
-              placeholder="スレッドタイトル"
-              defaultValue=""
+              onChange={(e) => {
+                setTitle(e.target.value);
+              }}
+              placeholder="タイトルを入力してください"
+              value={title}
               required
             />
-          </div>
-          <button className={styles.buttonNeumorphism}>作成</button>
-        </label>
-      </form>
+            <button className={styles.button}>作成</button>
+          </label>
+        </form>
+      </div>
     </section>
   );
 }
