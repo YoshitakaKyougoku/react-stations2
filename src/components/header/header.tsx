851c6cbb0f9bf7512add6297a94a9bf7e8895516
YoshitakaKyougoku <114034035+YoshitakaKyougoku@users.noreply.github.com>
2024-05-02 14:03:45 +0900

    テキストに架線を引く

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,10 +1,18 @@
-import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
+import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
 import styles from "./header.module.css";
 function Header() {
   return (
     <header className={styles.container}>
-      <Link to='/' className={styles.home}>掲示板</Link>
-      <Link to='/thread/new'>スレッドを立てる</Link>
+      <Link to="/" className={styles.home}>
+        <p className={styles.textWrapper}>
+          <span className={styles.backgroundImage}>掲示板</span>
+        </p>
+      </Link>
+      <Link to="/thread/new">
+        <p className={styles.textWrapper}>
+          <span className={styles.backgroundImage}>スレッドを立てる</span>
+        </p>
+      </Link>
     </header>
   );
 }
